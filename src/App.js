ecd7120abe7f0de9c7c6e25deca6e36f032333e5
kazan94 <kazan94@ya.ru>
2024-03-12 22:22:26 +0300

    App.js-добавил header, добавил logo & favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div className="App">Hello World!!!</div>;
+  return <div className="wrapper">
+     <header>
+      <div className="headerLeft">
+        <img width={90} height={90} src="/img/logo.png" alt="Логотип LOGO-SHOP" />
+        <div>
+          <h3>LOGO-SHOP</h3>
+          <p>Магазин лучших логотипов</p>
+        </div>
+      </div>
+      <ul className="headerRight">
+        <li>
+        <img width={24} height={24} src="/img/cart.svg" alt="Корзина" />
+          <span>1205 руб.</span>
+        </li>
+        <li>
+        <img width={24} height={24} src="/img/profile.svg" alt="Профиль" />
+        </li>
+      </ul>
+     </header> 
+     {<div className="content">
+      <h1>Все логотипы</h1>
+      ....
+     </div>}
+  </div>;
 }
 
-export default App;
+export default App; 
